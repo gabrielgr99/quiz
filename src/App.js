d9650f8ab3ce6f54374055744be754d0314e79bd
Gabriel Gomes Rodrigues <gabrielbj99@gmail.com>
2023-03-07 19:28:52 -0300

    docs: Adiciona configs do tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import End from './components/End';
+import PageHeader from './components/PageHeader';
+import Questions from './components/Questions';
 
 function App() {
+  const [finishQuestions, setFinishQuestion] = useState(true);
+  const [rightAnswers, setRightAnswers] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="h-[100vh] bg-gradient-to-br from-[#3d3aff] via-[#C25EFF]
+      to-[#FF5AEF]"
+    >
+      <PageHeader />
+      {
+        finishQuestions
+        ? <Questions setFinishQuestion={setFinishQuestion} rightAnswers={rightAnswers} setRightAnswers={setRightAnswers} />
+        : <End rightAnswers={rightAnswers} />
+      }
     </div>
   );
 }
